da7bfb01a0d96d8640f556d28db071c1ec5481e3
zouxinyao <zouxinyao@outlook.com>
2021-05-05 22:51:53 +0800

    unit02+unit03

--- 02_context/go.mod
+++
@@ -1,7 +0,0 @@
-module example
-
-go 1.15
-
-require gee v0.0.0
-
-replace github.com\ZouXinyao\learn_gee\02_context\gee => ./gee
---
+++ 03_trie/gee/context.go
@@ -0,0 +1,70 @@
+package gee
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type H map[string]interface{}
+
+type Context struct {
+	Writer     http.ResponseWriter
+	Req        *http.Request
+	Path       string
+	Method     string
+	StatusCode int
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
+
+func (context *Context) PostForm(key string) string {
+	return context.Req.FormValue(key)
+}
+
+func (context *Context) Query(key string) string {
+	return context.Req.URL.Query().Get(key)
+}
+
+func (context *Context) Status(code int) {
+	context.StatusCode = code
+	context.Writer.WriteHeader(code)
+}
+
+func (context *Context) SetHeader(key string, value string) {
+	context.Writer.Header().Set(key, value)
+}
+
+// 快速构造String/Data/JSON/HTML响应
+func (context *Context) String(code int, format string, value ...interface{}) {
+	context.SetHeader("Content-Type", "text/plain")
+	context.Status(code)
+	context.Writer.Write([]byte(fmt.Sprintf(format, value...)))
+}
+
+func (context *Context) JSON(code int, obj interface{}) {
+	context.SetHeader("Content-Type", "text/plain")
+	context.Status(code)
+	encoder := json.NewEncoder(context.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(context.Writer, err.Error(), 500)
+	}
+}
+
+func (context *Context) Data(code int, data[]byte) {
+	context.Status(code)
+	context.Writer.Write(data)
+}
+
+func (context *Context) HTML(code int, html string) {
+	context.SetHeader("Content-Type", "text/html")
+	context.Status(code)
+	context.Writer.Write([]byte(html))
+}
